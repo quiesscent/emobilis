9996316b2f3b9a6511abd30a8036d97a2656d921
quiesscent <ciphedc@gmail.com>
2025-03-01 22:55:11 +0300

    changes

--- skeleton/skeleton/settings.py
+++ skeleton/skeleton/settings.py
@@ -147,8 +147,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR / 'static'
-STATICFILES_DIRS = [BASE_DIR / 'staticfiles',]
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_DIRS = [BASE_DIR / 'static',]
 
 
 
